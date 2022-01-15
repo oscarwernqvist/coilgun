b2812c00fe416a7cfe9ddc873d8ad1167f5f04a2
Simon Paulsson <simon@paulsson.one>
2022-01-15 17:12:56 +0100

    added coil and power source class

--- src/coilgun/coil.py
+++ src/coilgun/coil.py
@@ -1,5 +1,63 @@
 import numpy as np
 
+from dataclasses import dataclass
+from .power_source import PowerSource
+from math_and_physics.constants import mu_0
+from math_and_physics.funcs import sign
 
-def Coil:
-	pass
+
+@dataclass
+class Coil():
+	"""Class representing a single coil in a coilgun"""
+
+	coils: list[int]  	 		# Number of coils for every 'circle'
+	inner_diameter: float 		# Inner diameter of the coil. [mm]
+	wire_diameter: float 		# Diameter of the wire used in the coil. [mm]
+	resistivity: float 			# Resistivity of the material used in the wire [Ohm x mm]
+	power_source: PowerSource 	# The power source used by the coil
+
+	def resistance(self):
+		"""Calculate the resistance in the coil"""
+
+		# Length of wire (can be calulated using the formula for the arithmetic seris)
+		a1 = self.inner_diameter + self.wire_diameter
+		d = 2 * self.wire_diameter
+		n = np.array(self.coils)
+
+		L = np.sum(n * (2 * a1 + (n-1) * d) ) * np.pi / 2.0
+
+		# Cross-sectional area of wire
+		A = np.pi * self.wire_diameter**2 / 4
+
+		# Resistance of wire
+		return self.resistivity * L / A
+
+	def B_field(self, z: float):
+		"""Calculate the B field at a coordinate z on the center axis of the coil. z=0 is the first 'circle'"""
+
+		# Current in the coil
+		I = self.power_source.current(self.resistance())
+
+		# The B field can be calculated using the formula for a current loop
+		B = 0
+		# Iterate over one 'circle' at a time
+		for i, n in enumerate(self.coils):
+			# Dist to current circle
+			dz = i * self.wire_diameter - z
+			# Radius for all coils in the current circle
+			a = np.array([self.inner_diameter + self.wire_diameter * (2*i + 1) for i in range(n)]) / 2
+			# Convert to meter
+			a = a * 1e-3
+			dz = dz * 1e-3
+
+			dB = mu_0 * I * np.sum(a**2 / np.power(dz**2 + a**2, 3/2)) / 2
+
+			B += sign(dz) * dB 	# Not sure about this sign(dz). It is not how the B field works
+
+			print(B * 2 / mu_0 / I)
+
+		return B
+
+
+
+		
---
+++ src/math_and_physics/funcs.py
@@ -0,0 +1,5 @@
+def sign(a: float) -> float:
+	"""Return the sign of a number. 1 for positive numbers and -1 for negative numbers. If the number is zero the function returns 1"""
+	if a >= 0:
+		return 1.0
+	return -1.0
